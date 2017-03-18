96212fb156836176c1b26b066eabcba6dcb5e7ef
Stephen Mitchell <stephenmitchell@stephens-mbp.attlocal.net>
2017-03-18 11:46:03 -0400

    update on background img

--- js/comics.js
+++ js/comics.js
@@ -1,7 +1,20 @@
-$.ajax({url: "https://gateway.marvel.com:443/v1/public/comics?limit=20&apikey=514aba9de2d628c8f5b8e097bfee3a26", 
+$.ajax({url: "https://gateway.marvel.com:443/v1/public/comics?limit=12&apikey=514aba9de2d628c8f5b8e097bfee3a26", 
         success: function(response){
         response.data.results.forEach(function(comic){
           $('#comics').append( `
+            <div class="comic col-md-3">
+              <h2 class="comicname">${comic.title}</h2>
+              <img class="comicimage" src="${comic.thumbnail.path}.${comic.thumbnail.extension}" />
+              <h3 class="comicprice">$${comic.prices[0].price}</h3>
+            </div>        
+          `);
+      })
+}});
+
+$.ajax({url: "https://gateway.marvel.com:443/v1/public/comics?limit=12&offset=12&apikey=514aba9de2d628c8f5b8e097bfee3a26", 
+        success: function(response){
+        response.data.results.forEach(function(comic){
+          $('#comics2').append( `
             <div class="comic col-md-3">
               <h2 class="comicname">${comic.title}</h2>
               <img class="comicimage" src="${comic.thumbnail.path}.${comic.thumbnail.extension}" />
