896ff59ed1dac9f1773a6e9067a6344b3bd32946
Stephen Mitchell <stephenmitchell@stephens-mbp.attlocal.net>
2017-03-19 20:28:14 -0400

    update on background img

--- js/characters.js
+++ js/characters.js
@@ -1,4 +1,4 @@
-$.ajax({url: "https://gateway.marvel.com:443/v1/public/characters?limit=12&apikey=514aba9de2d628c8f5b8e097bfee3a26", 
+$.ajax({url: "https://gateway.marvel.com:443/v1/public/characters?limit=8&apikey=514aba9de2d628c8f5b8e097bfee3a26", 
         success: function(response){
         response.data.results.forEach(function(comic){
           $('#characters').append( `
@@ -11,7 +11,7 @@ $.ajax({url: "https://gateway.marvel.com:443/v1/public/characters?limit=12&apike
       })
 }});
 
-$.ajax({url: "https://gateway.marvel.com:443/v1/public/characters?limit=12&offset=12&apikey=514aba9de2d628c8f5b8e097bfee3a26", 
+$.ajax({url: "https://gateway.marvel.com:443/v1/public/characters?limit=8&offset=8&apikey=514aba9de2d628c8f5b8e097bfee3a26", 
         success: function(response){
         response.data.results.forEach(function(comic){
           $('#characters2').append( `
@@ -24,9 +24,19 @@ $.ajax({url: "https://gateway.marvel.com:443/v1/public/characters?limit=12&offse
       })
 }});
 
-$("body").on("click",".learnmore",function(response){
-    response.data.results.forEach(function(comic)
-    $("#myModal").modal()
-})
+$.ajax({url: "https://gateway.marvel.com:443/v1/public/characters?limit=8&offset=16&apikey=514aba9de2d628c8f5b8e097bfee3a26", 
+        success: function(response){
+        response.data.results.forEach(function(comic){
+          $('#characters3').append( `
+            <div class="comic col-md-3">
+              <h2 class="charactername">${comic.name}</h2>
+              <img class="characterphoto" src="${comic.thumbnail.path}.${comic.thumbnail.extension}" />
+              <h3 class="learnmore">Learn More</h3>
+            </div>        
+          `);
+      })
+}});
+
+
 
     // Then just just say that you want your modal to show up. So if your modal has an id of myModal
